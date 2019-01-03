97a5644e8059504ff650a67a7db14feb6de0e2df
Taeyoung Kim <taeyokim@gmail.com>
2019-01-03 17:58:40 +0900

    initial commit after local test

---
+++ StockTick/Hub/StockTickerHub.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace StockTick
+{
+    public class StockTickerHub : Hub
+    {
+        private readonly StockTicker _stockTicker;
+
+
+        public StockTickerHub(StockTicker stockTicker)
+        {
+            _stockTicker = stockTicker;
+        }
+
+        public IEnumerable<Stock> GetAllStocks()
+        {
+            return _stockTicker.GetAllStocks();
+        }
+
+        public ChannelReader<Stock> StreamStocks()
+        {
+            return _stockTicker.StreamStocks().AsChannelReader(10);
+        }
+
+        public string GetMarketState()
+        {
+            return _stockTicker.MarketState.ToString();
+        }
+
+        public async Task OpenMarket()
+        {
+            await _stockTicker.OpenMarket();
+        }
+
+        public async Task CloseMarket()
+        {
+            await _stockTicker.CloseMarket();
+        }
+
+        public async Task Reset()
+        {
+            await _stockTicker.Reset();
+        }
+    }
+}
---
+++ StockTick/Startup.cs
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StockTick
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<StockTicker>();
+
+            services.AddSignalR()
+                    .AddAzureSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+            app.UseFileServer();
+            app.UseAzureSignalR(routes =>
+            {
+                routes.MapHub<StockTickerHub>("/stock");
+            });
+        }
+    }
+}
