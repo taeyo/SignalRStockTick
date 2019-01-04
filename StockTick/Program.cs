3a7bfdb99225ceddffbdf7841b9537c412fe71b0
Taeyoung Kim <taeyokim@gmail.com>
2019-01-04 10:06:35 +0900

    added keyvault code

--- StockTick/Program.cs
+++ StockTick/Program.cs
@@ -1,8 +1,18 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Logging;
 
 namespace StockTick
 {
@@ -10,11 +20,27 @@ namespace StockTick
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var keyVaultEndpoint = GetKeyVaultEndpoint();
+                    if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                    {
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                        builder.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                    }
+                })
+                .UseStartup<Startup>()
+                .Build();
+
+        private static string GetKeyVaultEndpoint() => Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
+        // 개발환경에서 KEYVAULT_ENDPOINT는 user secrets에 있음. secrets에.json
+        // Azure App에서는 application settings에 있음.
+        // Azure App과 KeyVault는 MSI로 연결되어 있기에, Azure:SignalR:ConnectionString 시크릿을 키볼트에 기입해두면 알아서 가져와진다.
     }
 }
